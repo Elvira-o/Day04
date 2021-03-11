5b6e3cf4609b04bd8a335d595627e4e58a4472cf
Elvira-o <elvira2021ot@gmail.com>
2021-03-11 20:48:06 +0100

    Declare String Variable

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+    var myFirstName = "Elvira";
+    var myLastName = "Omerovic";
+    var myHomeTown = "Tuzla"
+
+     return {
+         
+         myFirstName,
+         myLastName,
+         myHomeTown
+        
+       
+    };
+}
+console.log(main());
+module.exports = main;
